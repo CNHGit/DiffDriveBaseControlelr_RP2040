2757821e75370ea90c26f70ccab0f7f8a8072ed7
nishadkulkarni <nishadkulkarni97@gmail.com>
2024-07-22 13:11:19 -0400

    PID parameter separated for both the wheels

--- App-DiffDrive/include/PIDParams.h
+++ App-DiffDrive/include/PIDParams.h
@@ -2,9 +2,13 @@
  * @brief PID CONTEXT ------------------------------------------------------------------
  */
 // PID Coefficients
-double _kP = 0.5;
-double _kI = 0.006;
-double _kD = 0.000000000625;
+double _kP_l = 0;//0.25;
+double _kI_l = 0;//0.006;
+double _kD_l = 0;//0.000000000625;
+
+double _kP_r = 1;//0.5;
+double _kI_r = 0;//0.006;
+double _kD_r = 0;//0.000000004073;//0.000000000625;
 
 // PID Data
 double rITerm = 0.0;
@@ -13,7 +17,6 @@ double lITerm = 0.0;
 // Current Speed
 int rLastSpeed = 0;
 int lLastSpeed = 0;
-
 
 // Current PWM
 uint8_t LEFT_PWM = 0;
