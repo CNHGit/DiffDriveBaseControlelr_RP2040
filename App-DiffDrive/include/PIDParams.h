14e2a2030abe7f65c3fbff9bd06f4d66991bd6a1
Kelebrimbor97 <nishadkulkarni97@gmail.com>
2025-08-15 11:48:13 -0400

    PID parameters that work for the main prototype wheelchair

--- App-DiffDrive/include/PIDParams.h
+++ App-DiffDrive/include/PIDParams.h
@@ -3,12 +3,12 @@
  */
 // PID Coefficients
 double _kP_l = 0.1;
-double _kI_l = 0.00;
-double _kD_l = 0.0;
+double _kI_l = 0.001;
+double _kD_l = 0.0001;
 
 double _kP_r = 0.1;
-double _kI_r = 0.00;
-double _kD_r = 0.0;//0.000000000625;
+double _kI_r = 0.001;
+double _kD_r = 0.0001;//0.000000000625;
 
 // PID Data
 double rITerm = 0.0;
@@ -17,6 +17,10 @@ double lITerm = 0.0;
 // Current Speed
 int rLastSpeed = 0;
 int lLastSpeed = 0;
+
+// Temp adjusted PWN
+double l_pwm = 0.0;
+double r_pwm = 0.0;
 
 // Current PWM
 uint8_t LEFT_PWM = 0;
